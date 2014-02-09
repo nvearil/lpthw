85761a37e2878ae9adc1e7ea85d1347bb3714120
twoism <nicholas.vearil@gmail.com>
2014-02-09 00:38:29 -0500

    dfsdfs

---
+++ ex21.py
@@ -0,0 +1,38 @@
+def add(a, b):
+	print "ADDING %d + %d" % (a, b)
+	return a + b
+
+def subtract(a, b):
+	print "SUBTRACTING %d - %d" % (a, b)
+	return a - b
+
+def multiply(a, b):
+	print "MULTIPLYING %d * %d" % (a, b)
+	return a * b
+
+def divide(a, b):
+	print "DIVIDING %d / %d" % (a, b)
+	return a / b
+
+
+print "Let's do some math with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+
+# A puzzle for the extra credit, type it in anyway.
+print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+radius = float(raw_input("radius> "))
+area_of_circle = multiply(3.14, multiply(radius, radius))
+
+print "That becomes: ", what, "Can you do it by hand?"
+print "A circle with a radius of %r has an area of %r" % (
+	radius, area_of_circle)
